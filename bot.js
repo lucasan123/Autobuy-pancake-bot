a8e440cb1bc4d6969b4b28594246e81e111782ef
lucasan <lucasan123sky@gmail.com>
2021-04-23 22:48:13 +0200

    some fixes

--- bot.js
+++ bot.js
@@ -25,11 +25,11 @@ const router = new ethers.Contract(
   ],
   account
 );
-console.log(Date() + 'BOT STARTED');
+console.log(Date() + '    BOT STARTED');
 
 factory.on('PairCreated', async (token0, token1, pairAddress) => {
-  console.log(Date() + `
-    New pair detected
+  console.log(Date() + `    New pair detected
+
     =================
     token0: ${token0}
     token1: ${token1}
@@ -58,8 +58,8 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
   const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
   //Our execution price will be a bit different, we need some flexbility
   const amountOutMin = amounts[1].sub(amounts[1].div(10));
-  console.log(Date() + `
-    Buying new token
+  try {
+    console.log(Date() + `    Buying new token
     =================
     tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
     tokenOut: ${amounOutMin.toString()} ${tokenOut}
@@ -74,4 +74,7 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
   const receipt = await tx.wait();
   console.log('Transaction receipt');
   console.log(receipt);
+} catch(error) { 
+console.log(Date() + '    Error buying, probably no liquidity');
+} 
 });
